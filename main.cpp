403e9a52c92085b078c0a8c593d8420c093b4cc1
Senior Hilter <75303620+allenabel03@users.noreply.github.com>
2023-08-30 22:32:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,105 +1,115 @@
 Allen Ace Abel
 Enrico Dizon
 Javier Bolong
-// Program to find correlation coefficient
-#include<bits/stdc++.h>
-#include<iostream>
-#include<vector>
-using namespace std;
-//testing pushing
-//COdeblock gaming testing
-// function that returns correlation coefficient.
-float crossCorrelation(float X_raw[], float Y_raw[], float n_X, float n_Y)
-{
-    //proc means processed
-    //proc = average - X or Y(n)
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class XYDataProcessor {
+public:
+    bool importFromFile(const std::string& filename, std::vector<float>& xValues, std::vector<float>& yValues) {
+        std::ifstream inputFile(filename);
+
+        if (!inputFile.is_open()) {
+            std::cerr << "Failed to open file: " << filename << std::endl;
+            return false;
+        }
+
+        float x, y;
+        while (inputFile >> x >> y) {
+            xValues.push_back(x);
+            yValues.push_back(y);
+        }
+
+        inputFile.close();
+        return true;
+    }
+};
+
+float crossCorrelation(const std::vector<float>& X_raw, const std::vector<float>& Y_raw, int n_X, int n_Y) {
     float average_X = 0, average_Y = 0, proc_X = 0, proc_Y = 0, counter = 0, hold = 0;
 
-    vector<float> list_proc_X = {};
-    vector<float> list_proc_Y = {};
+    std::vector<float> list_proc_X = {};
+    std::vector<float> list_proc_Y = {};
 
-    vector<float> cross_X = {};
-    vector<float> cross_Y = {};
+    std::vector<float> cross_X = {};
+    std::vector<float> cross_Y = {};
 
-    vector<float> r_XY = {};
+    std::vector<float> r_XY = {};
 
-    //loops for averages
-    for (int i = 0; i < n_X; i++)
-    {
+    for (int i = 0; i < n_X; i++) {
         average_X += X_raw[i];
     }
     average_X = average_X / n_X;
 
-    for (int i = 0; i < n_Y; i++)
-    {
+    for (int i = 0; i < n_Y; i++) {
         average_Y += Y_raw[i];
     }
     average_Y = average_Y / n_Y;
 
-    cout << "Average of list X: " << average_X << endl;
-    cout << "Average of list Y: " << average_Y << endl;
+    std::cout << "Average of list X: " << average_X << std::endl;
+    std::cout << "Average of list Y: " << average_Y << std::endl;
 
-    // obtaining X(n), and Y(n)
-    for (int j = 0; j < n_X; j++)
-    {
+    for (int j = 0; j < n_X; j++) {
         proc_X = X_raw[j] - average_X;
         list_proc_X.push_back(proc_X);
-        cout << proc_X << ", ";
+        std::cout << proc_X << ", ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
-    for (int j = 0; j < n_Y; j++)
-    {
+    for (int j = 0; j < n_Y; j++) {
         proc_Y = Y_raw[j] - average_Y;
         list_proc_Y.push_back(proc_Y);
-        cout << proc_Y << ", ";
+        std::cout << proc_Y << ", ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
-    //checking which line
-    if (list_proc_X.size() > list_proc_Y.size())
-    {
+    if (list_proc_X.size() > list_proc_Y.size()) {
         counter = list_proc_X.size();
-    }
-    else
-    {
+    } else {
         counter = list_proc_Y.size();
     }
 
-    for(int k = 0; k < counter; k++)
-    {
+    for (int k = 0; k < counter; k++) {
         hold = 0;
 
-        for(int l = 0; l < k; l++)
-        {
+        for (int l = 0; l < k; l++) {
             hold += list_proc_X[0 + k] * list_proc_Y[counter - l];
         }
 
         r_XY.push_back(hold);
     }
-    cout << "r_xy sequence: ";
-    for (int a = 0; a < r_XY.size(); a++)
-    {
-
-        cout << r_XY[a] << ", ";
+    std::cout << "r_xy sequence: ";
+    for (int a = 0; a < r_XY.size(); a++) {
+        std::cout << r_XY[a] << ", ";
     }
-    cout << endl;
+    std::cout << std::endl;
     return 0;
 }
 
-int main()
-{
+int main() {
+    XYDataProcessor processor;
+    std::vector<float> X_values;
+    std::vector<float> Y_values;
 
-    float X[] = {1, 5, 10, 3, 5};
-    float Y[] = {4, 2, 20, 3, -5};
+    if (processor.importFromFile("xyvalues.txt", X_values, Y_values)) {
+        std::cout << "Import successful.\n";
 
-    //Find the size of array.
-    int n_X = sizeof(X)/sizeof(float);
-    int n_Y = sizeof(Y)/sizeof(float);
+        int n_X = X_values.size();
+        int n_Y = Y_values.size();
+        float X[n_X];
+        float Y[n_Y];
+        for (int i = 0; i < n_X; i++) {
+            X[i] = X_values[i];
+        }
+        for (int i = 0; i < n_Y; i++) {
+            Y[i] = Y_values[i];
+        }
 
-    //Function call to crossCorrelation.
-    crossCorrelation(X, Y, n_X, n_Y);
+        crossCorrelation(X, Y, n_X, n_Y);
+    } else {
+        std::cerr << "Import failed." << std::endl;
+    }
 
-    cout << "successful transfer!" << endl;
     return 0;
 }
